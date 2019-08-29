3119ef54c3bfdda2e4879ea4413eda5212c887e1
Dylan Fox <dylan-fox@users.noreply.github.com>
2019-08-29 13:58:59 -0700

    Initial Commit
    HoloLens 2 Deployable AR for VIPs

---
+++ Assets/Scripts/ControlScript2.cs
@@ -0,0 +1,73 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.XR.WSA.Input;
+
+public class ControlScript2 : MonoBehaviour
+{
+    #region **INITIALIZATION**
+
+
+    [Tooltip("This text will change to copy the Debug output.")]
+    public GameObject debugText;
+    private string output = ""; //helps print text in UI
+
+
+
+    void Start()
+    {
+        ClearDebug();
+    }
+
+    private void Update()
+    {
+    }
+
+    private void OnEnable()
+    {
+        Application.logMessageReceived += HandleLog; //helps print text in UI
+    }
+
+    void OnDisable()
+    {
+
+        Application.logMessageReceived -= HandleLog;
+    }
+
+    #endregion
+
+    #region HOLOLENS DEBUG UI
+
+    void HandleLog(string logString, string stackTrace, LogType type)
+    {
+        output = logString;
+        if (debugText.GetComponent<TextMesh>().text.Length <= 2000)
+        {
+            debugText.GetComponent<TextMesh>().text += "\n" + output;
+        }
+        
+    }
+
+    public void ClearDebug()
+    {
+        debugText.GetComponent<TextMesh>().text = "Debug log cleared.";
+    }
+
+    public void ToggleDebug()
+    {
+        //Toggles active status of Debug Window.
+        if (debugText.gameObject.transform.parent.gameObject.activeSelf)
+        {
+            debugText.gameObject.transform.parent.gameObject.SetActive(false);
+            Debug.Log("Debug window deactivated.");
+        }
+
+        else
+        {
+            debugText.gameObject.transform.parent.gameObject.SetActive(true);
+            Debug.Log("Debug window reactivated.");
+        }
+    }
+
+    #endregion
+}
---
+++ Assets/Scripts/ObstacleSonification.cs
@@ -0,0 +1,87 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ObstacleSonification : MonoBehaviour
+{
+    [Tooltip("Game object to use as obstacle beacon.")]
+    public GameObject obstacleBeacon;
+
+    [Tooltip("Maximum distance to sonify obstacles.")]
+    public float maxDist = 5;
+
+    public bool obstacleMode = false;
+    private float detectionRadius = 0.05f;
+
+    public float distance;
+
+    void Start()
+    {
+        
+    }
+
+    void Update()
+    {
+        //While obstacle mode is on, move the obstacle beacon to where the user's gaze is, or straight out 10m if no hit is detected.
+        if (obstacleMode)
+        {
+            var headPosition = Camera.main.transform.position;
+            var gazeDirection = Camera.main.transform.forward;
+
+            //Debug.Log("Head position: " + headPosition);
+            //Debug.Log("Gaze Direction: " + gazeDirection);
+
+            RaycastHit hit;
+            if (Physics.SphereCast(headPosition, detectionRadius, gazeDirection, out hit, maxDist)) {
+                obstacleBeacon.transform.position = hit.point;
+                distance = Vector3.Distance(headPosition, hit.point);
+                //Debug.Log("Hit! Position = " + obstacleBeacon.transform.position);
+            }
+
+            else
+            {
+                obstacleBeacon.transform.position = headPosition + gazeDirection * maxDist;
+                distance = Vector3.Distance(headPosition, hit.point);
+                //Debug.Log("Miss. Position = " + obstacleBeacon.transform.position);
+            }
+
+            //Change pitch based on distance
+            obstacleBeacon.GetComponent<AudioSource>().pitch = 0.5 + (10f - distance)/5;
+        }
+    }
+
+    public void ObstaclesOn()
+    {
+        if (!obstacleMode)
+        {
+            //Toggle on obstacle mode.
+            obstacleMode = true;
+
+            //Activate obstacle beacon
+            obstacleBeacon.SetActive(true);
+
+            Debug.Log("Obstacle mode on.");
+
+        }
+
+        //Play "Obstacles on" speech
+        //insert code
+    }
+
+    public void ObstaclesOff()
+    {
+        if (obstacleMode)
+        {
+            obstacleMode = false;
+
+            //Deactivate obstacle beacon
+            obstacleBeacon.SetActive(false);
+
+            Debug.Log("Obstacle mode off.");
+
+        }
+
+        //Play "Obstacles off" speech
+        //insert code
+    }
+}
